a82f7fe5dfa4a89c0d6352b489063120424fb4ea
Marat <447599489marat@gmail.com>
2024-06-16 21:20:05 +0300

    404 page added.

--- Diary.Api/Middlewares/ExceptionHandlingMiddleware.cs
+++ Diary.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 using Diary.Domain.Result;
 using Newtonsoft.Json;
 using IFormattable = System.IFormattable;
@@ -10,7 +11,7 @@ public class ExceptionHandlingMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger _logger;
- 
+
     public ExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
     {
         _next = next;
@@ -33,18 +34,25 @@ public class ExceptionHandlingMiddleware
                 string responseBody = new StreamReader(swapStream).ReadToEnd();
                 swapStream.Seek(0, SeekOrigin.Begin);
 
-                if (httpContext.Response.StatusCode == (int)HttpStatusCode.BadRequest)
+                switch (httpContext.Response.StatusCode)
                 {
-                    dynamic data = JsonConvert.DeserializeObject(responseBody);
-                    string errorMessage = data.errorMessage;
-                    _logger.Warning(new ArgumentException("Bad request: "+errorMessage), errorMessage);
+                    case (int)HttpStatusCode.BadRequest:
+                        dynamic data = JsonConvert.DeserializeObject(responseBody);
+                        string errorMessage = data.errorMessage;
+                        _logger.Warning(new ArgumentException("Bad request: " + errorMessage), errorMessage);
+                        break;
+                    case (int)HttpStatusCode.NotFound:
+                        httpContext.Response.ContentType = "text/html";
+                        const string pageNotFoundImage = "<img style=\"width: 100%; height: 100%;\" src=\"https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-template-3.png\" alt=\"404. We are sorry, but the page you requested was not found.\"/>";
+                        await WriteToStream(swapStream, pageNotFoundImage);
+                        break;
                 }
 
                 await swapStream.CopyToAsync(originalResponseBody);
                 httpContext.Response.Body = originalResponseBody;
             }
         }
-        catch(Exception exception)
+        catch (Exception exception)
         {
             await HandleExceptionAsync(httpContext, exception);
         }
@@ -55,18 +63,18 @@ public class ExceptionHandlingMiddleware
         _logger.Error(exception, exception.Message);
 
         var errorMessage = exception.Message;
-        
+
         var response = exception switch
         {
             UnauthorizedAccessException _ => new BaseResult
             {
-                ErrorMessage = errorMessage, 
+                ErrorMessage = errorMessage,
                 ErrorCode = (int)HttpStatusCode.Unauthorized
             },
-            
+
             _ => new BaseResult
             {
-                ErrorMessage = "Internal server error: "+errorMessage,//might be hardcode
+                ErrorMessage = "Internal server error: " + errorMessage, //might be hardcode
                 ErrorCode = (int)HttpStatusCode.InternalServerError
             }
         };
@@ -76,4 +84,11 @@ public class ExceptionHandlingMiddleware
         httpContext.Response.StatusCode = (int)response.ErrorCode;
         await httpContext.Response.WriteAsJsonAsync(response);
     }
+    private async Task WriteToStream(MemoryStream stream, string message)
+     {
+         var buffer = Encoding.UTF8.GetBytes(message);
+         await stream.WriteAsync(buffer, 0, buffer.Length);
+         stream.Seek(0, SeekOrigin.Begin);
+     }
 }
+
