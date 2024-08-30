6d72b903c2c156140216cb20d045602e3efa1a9d
Marat <dotnetcoder3@gmail.com>
2024-08-30 11:26:33 +0300

    Code reformatted.

--- Diary.Domain/Extensions/DateStringExtension.cs
+++ Diary.Domain/Extensions/DateStringExtension.cs
@@ -2,8 +2,5 @@ namespace Diary.Domain.Extensions;
 
 public static class DateStringExtension
 {
-    public static string ToLongUtcString(this DateTime dateTime)
-    {
-        return dateTime.ToString("(UTC): " + "dd.MM.yyyy HH:mm");
-    }
+    public static string ToLongUtcString(this DateTime dateTime) => dateTime.ToString("(UTC): " + "dd.MM.yyyy HH:mm");
 }
