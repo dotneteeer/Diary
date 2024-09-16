a5c95736f9308da2a99b5f27238b85e8f1a80d2a
Marat <447599489marat@gmail.com>
2024-09-16 19:36:40 +0300

    Route to AuthController changed( added [Route("api/[controller]")])

--- Diary.Api/Controllers/AuthController.cs
+++ Diary.Api/Controllers/AuthController.cs
@@ -13,6 +13,7 @@ namespace Diary.Api.Controllers;
 /// <response code="400">If user was not registrated/logined</response>
 /// <response code="500">If internal server error occured</response>
 [ApiController]
+[Route("api/[controller]")]
 [ProducesResponseType(StatusCodes.Status200OK)]
 [ProducesResponseType(StatusCodes.Status400BadRequest)]
 [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -24,14 +25,14 @@ public class AuthController : Controller
     {
         _authService = authService;
     }
-    
+
     /// <summary>
     /// user registartion
     /// </summary>
     /// <param name="dto"></param>
     /// <returns></returns>
     [HttpPost("register")]
-    public async Task<ActionResult<BaseResult>> Register([FromBody]RegisterUserDto dto)
+    public async Task<ActionResult<BaseResult>> Register([FromBody] RegisterUserDto dto)
     {
         var response = await _authService.Register(dto);
         if (response.IsSuccess)
@@ -41,14 +42,14 @@ public class AuthController : Controller
 
         return BadRequest(response);
     }
-    
+
     /// <summary>
     /// user login
     /// </summary>
     /// <param name="dto"></param>
     /// <returns></returns>
     [HttpPost("login")]
-    public async Task<ActionResult<BaseResult<TokenDto>>> Login([FromBody]LoginUserDto dto)
+    public async Task<ActionResult<BaseResult<TokenDto>>> Login([FromBody] LoginUserDto dto)
     {
         var response = await _authService.Login(dto);
         if (response.IsSuccess)
