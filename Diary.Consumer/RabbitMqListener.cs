305bb706e138ff8aff1b9c48142b4c5cad40605e
Marat <447599489marat@gmail.com>
2024-11-02 10:18:35 +0300

    Actual rabbitmq config is in appsettings.json

--- Diary.Consumer/RabbitMqListener.cs
+++ Diary.Consumer/RabbitMqListener.cs
@@ -6,15 +6,14 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Serilog;
 
-
 namespace Diary.Consumer;
 
 public class RabbitMqListener : BackgroundService
 {
-    private readonly IConnection _connection;
     private readonly IModel _channel;
-    private readonly IOptions<RabbitMqSettings> _options;
+    private readonly IConnection _connection;
     private readonly ILogger _logger;
+    private readonly IOptions<RabbitMqSettings> _options;
 
     public RabbitMqListener(IOptions<RabbitMqSettings> options, ILogger logger)
     {
@@ -29,19 +28,18 @@ public class RabbitMqListener : BackgroundService
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         stoppingToken.ThrowIfCancellationRequested();
-        
+
         var consumer = new EventingBasicConsumer(_channel);
         consumer.Received += (obj, basicDeliver) =>
         {
             var content = Encoding.UTF8.GetString(basicDeliver.Body.ToArray());
-            _logger.Information("Message received: "+content);
+            _logger.Information("Message received: " + content);
             _channel.BasicAck(basicDeliver.DeliveryTag, false);
-            
         };
         _channel.BasicConsume(_options.Value.QueueName, false, consumer);
         return Task.CompletedTask;
     }
-    
+
     //docker run -d --hostname my-rabbit --name some-rabbit -p 5672:5672 -p 15672:15672 rabbitmq:3-management
-    //create exchange, queue and bind them with names in launchSettings.json
+    //create exchange, queue and bind them with names in appsettings.json
 }
