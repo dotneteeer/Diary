2fff2d2721a1796f93d4665112297eefeb47a9fa
Marat <447599489marat@gmail.com>
2024-12-15 10:32:44 +0300

    Unit test ReportServiceFactory.cs is updated and now unit tests are not dependent on service like rabbitmq  or redis

--- Diary.Tests/UnitTests/Factories/ReportServiceFactory.cs
+++ Diary.Tests/UnitTests/Factories/ReportServiceFactory.cs
@@ -1,13 +1,13 @@
-using System.Reflection;
 using AutoMapper;
 using Diary.Application.Services;
 using Diary.Application.Validation;
 using Diary.Domain.Entity;
 using Diary.Domain.Interfaces.Repositories;
+using Diary.Domain.Interfaces.Services;
 using Diary.Domain.Settings;
+using Diary.Producer.Interfaces;
 using Diary.Tests.UnitTests.Configurations;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Moq;
 using MapperConfiguration = Diary.Tests.UnitTests.Configurations.MapperConfiguration;
@@ -17,11 +17,11 @@ namespace Diary.Tests.UnitTests.Factories;
 public class ReportServiceFactory
 {
     public static readonly Mock<IOptions<RabbitMqSettings>> MockRabbitMqOptions = new();
-    private readonly ReportService _reportService;
+    private readonly IReportService _reportService;
     public readonly IMapper Mapper = MapperConfiguration.GetMapperConfiguration();
 
     public readonly Mock<IDistributedCache> MockDistributedCache = new();
-    public readonly Mock<Producer.Producer> MockMessageProducer = new();
+    public readonly Mock<IMessageProducer> MockMessageProducer = new();
 
     public readonly Mock<IBaseRepository<Report>> MockReportRepository =
         MockRepositoriesGetter.GetMockReportRepository();
@@ -32,38 +32,23 @@ public class ReportServiceFactory
     public readonly Mock<ReportValidator> MockValidator = new();
 
 
-    static ReportServiceFactory()
-    {
-        #region Getting root directory
-
-        var projectDirectory = Directory.GetParent(AppContext.BaseDirectory); //path to runtime directory
-        var projectName =
-            Assembly.GetExecutingAssembly().GetName().Name!.Split('.')
-                .First(); //name of app if naming is of type '<AppName>.<DirectoryName>' (e. g. 'Diary.Api')
-        while (projectDirectory!.Name != projectName)
-            projectDirectory = projectDirectory.Parent!; //getting path to root directory
-
-        #endregion
-
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(projectDirectory.FullName)
-            .AddJsonFile(projectName + ".Api/appsettings.json", optional: false, reloadOnChange: false)
-            .Build();
-
-        var rabbitMqSettings = new RabbitMqSettings();
-        configuration.GetSection(nameof(RabbitMqSettings)).Bind(rabbitMqSettings);
-        var options = Options.Create(rabbitMqSettings);
-
-        MockRabbitMqOptions.Setup(o => o.Value).Returns(options.Value);
-    }
-
     public ReportServiceFactory()
     {
+        //the following rabbitmq options values are test (random) and have no relations with real ones
+        MockRabbitMqOptions.Setup(o => o.Value).Returns(new RabbitMqSettings
+        {
+            QueueName = "queue",
+            RoutingKey = "diary.topic",
+            ExchangeKey = "diary.exchange",
+            HostName = "localhost",
+            Port = 5672
+        });
+
         _reportService = new ReportService(MockReportRepository.Object,
             MockUserRepository.Object,
             MockValidator.Object, Mapper, MockRabbitMqOptions.Object, MockMessageProducer.Object,
             MockDistributedCache.Object, null!);
     }
 
-    public ReportService GetReportService() => _reportService;
+    public IReportService GetReportService() => _reportService;
 }
