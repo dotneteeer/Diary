00fc5af984038e9201847c3712ce839e4d714fd5
Marat <447599489marat@gmail.com>
2025-01-02 11:25:55 +0300

    builder.HasData removed

--- Diary.DAL/Configurations/RoleConfiguration.cs
+++ Diary.DAL/Configurations/RoleConfiguration.cs
@@ -10,24 +10,5 @@ public class RoleConfiguration : IEntityTypeConfiguration<Role>
     {
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
         builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
-
-        builder.HasData(new List<Role>
-        {
-            new()
-            {
-                Id = 1,
-                Name = "User"
-            },
-            new()
-            {
-                Id = 2,
-                Name = "Admin"
-            },
-            new()
-            {
-                Id = 3,
-                Name = "Moderator"
-            }
-        });
     }
 }
